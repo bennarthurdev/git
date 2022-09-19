8c39b96e2c08ac372341f764426291d1dfa100ea
Benn Arthur <87674915+bennarthurdev@users.noreply.github.com>
2022-09-19 16:34:45 -0300

    Create a first version of login validation

---
+++ login.py
@@ -0,0 +1,15 @@
+def validateUsername(bool = False):
+  print("Here a code to validate the username")
+  return bool
+def validatePassword(bool = False):
+  print("Validate a password code")
+  return bool
+  
+def login(func1 , func2):
+  if func1() == True and func2() == True
+    status = 200
+  else 
+    status = 'error'
+  
+  
+  return status
